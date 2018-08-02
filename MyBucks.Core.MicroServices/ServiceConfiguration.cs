4fc8b3f16aa302da62261abeafefa5058c7605e0
Ruan Strydom <ruans@mybucks.com>
2018-08-02 14:11:42 +0200

    Fix auto mapper injection

--- MyBucks.Core.MicroServices/ServiceConfiguration.cs
+++ MyBucks.Core.MicroServices/ServiceConfiguration.cs
@@ -1,8 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using AutoMapper;
 using Microsoft.Extensions.Configuration;
 using MyBucks.Core.MicroServices.Abstractions;
+using MyBucks.Core.MicroServices.Mappers;
 using MyBucks.Core.Model.Abstractions;
 using SimpleInjector;
 
@@ -48,6 +52,36 @@ namespace MyBucks.Core.MicroServices
         public void InjectServiceBase()
         {
             _container.Collection.Register(typeof(IServiceBase), typeof(IServiceBase).Assembly);
+        }
+
+        public void InjectAutoMapper(Assembly currentAssembly)
+        {
+            // Assume we have multiple Profile classes.  We'll load them individually to create multiple mappers for our factory
+            var mapperFactory = new MapperFactory();
+            IMapper defaultMapper = null;
+            var types = currentAssembly.GetTypes().Where(x => x.GetTypeInfo().IsClass && x.IsAssignableFrom(x) && x.GetTypeInfo().BaseType == typeof(Profile)).ToList();
+            foreach (var type in types)
+            {
+                var profileName = string.Empty;
+                var config = new AutoMapper.MapperConfiguration(cfg =>
+                {
+                    var profile = (Profile)Activator.CreateInstance(type);
+                    profileName = profile.ProfileName;
+                    cfg.AddProfile(profile);
+                });
+
+                var mapper = config.CreateMapper();
+                mapperFactory.Mappers.Add(profileName, mapper);
+
+                // If we still want normal functionality with a default injected IMapper
+                if (defaultMapper == null)
+                {
+                    defaultMapper = mapper;
+                    _container.Register(() => defaultMapper);
+                }
+            }
+
+            _container.Register<IMapperFactory>(() => mapperFactory);
         }
     }
 }
