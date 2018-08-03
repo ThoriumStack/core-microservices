b82025dc8f1a0d13159efd683711c49061c21c06
Ruan Strydom <ruans@mybucks.com>
2018-08-03 08:33:07 +0200

    Create generic repository

---
+++ MyBucks.Core.MicroServices/Abstractions/IGenericRepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿using MyBucks.Core.DataProvider;
+
+namespace MyBucks.Core.MicroServices.Abstractions
+{
+    public interface IGenericRepositoryBase<TDbContext> : IRepositoryBase
+        where TDbContext : ContextBase
+    {
+        TDbContext CreateDbContext();
+
+        new TDbContext CurrentDbContext { get; set; }
+    }
+}
---
+++ MyBucks.Core.MicroServices/Repositories/GenericRepositoryBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using AutoMapper;
+using MyBucks.Core.DataProvider;
+using MyBucks.Core.MicroServices.Abstractions;
+using MyBucks.Core.MicroServices.ConfigurationModels;
+using Serilog;
+
+namespace MyBucks.Core.MicroServices.Repositories
+{
+    public abstract class GenericRepositoryBase<TDbContext> : RepositoryBase, IGenericRepositoryBase<TDbContext>
+        where TDbContext : ContextBase
+    {
+        private readonly DbSettings _dbSettings;
+        
+        protected GenericRepositoryBase(DbSettings dbSetting, IMapper mapper, ILogger logger) : base (mapper, logger)
+        {
+            _dbSettings = dbSetting;
+        }
+        
+        public virtual TDbContext CreateDbContext()
+        {
+            TDbContext context;
+            if (!string.IsNullOrWhiteSpace(_dbSettings.ConnectionString))
+                context = (TDbContext) Activator.CreateInstance(typeof(TDbContext), _dbSettings.ConnectionString);
+            else
+                context = Activator.CreateInstance<TDbContext>();
+            
+            CurrentDbContext = context;
+            return context;
+        }
+
+        private TDbContext _context;
+
+        public new virtual TDbContext CurrentDbContext
+        {
+            get => _context;
+            set
+            {
+                _context = value;
+                base.CurrentDbContext = value;
+            }
+        }
+    }
+}
