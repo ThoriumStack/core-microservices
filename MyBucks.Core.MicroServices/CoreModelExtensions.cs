723b191773d3c70a5906b90e5200d0d9857b626d
Tjaart Blignaut <tjaart@getbucks.com>
2019-02-12 17:31:52 +0200

    remove core.defensive in favour of thorium package

--- MyBucks.Core.MicroServices/CoreModelExtensions.cs
+++ MyBucks.Core.MicroServices/CoreModelExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using MyBucks.Core.Defensive;
 using MyBucks.Core.Model;
+using Thorium.FluentDefense;
 
 namespace MyBucks.Core.MicroServices
 {
